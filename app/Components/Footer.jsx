02a925163be197338f1cac2432a1a4d0610ffdf0
Varsani2520 <varsaniranjani@gmail.com>
2024-01-29 19:36:18 +0530

    final

--- app/Components/Footer.jsx
+++ app/Components/Footer.jsx
@@ -1,59 +1,313 @@
-import React from 'react';
-import { Container, Grid, Typography } from '@mui/material';
-import Link from '@mui/material/Link';
+import React from "react";
+import {
+  Container,
+  Grid,
+  Typography,
+  TextField,
+  IconButton,
+  InputAdornment,
+  Button,
+} from "@mui/material";
+import Link from "@mui/material/Link";
+import { Facebook, Twitter, LinkedIn } from "@mui/icons-material";
 
 const Footer = () => {
   return (
-    <footer style={{ background: 'hotpink', padding: '20px' }}>
+    <footer style={{ background: "red", padding: "20px" }}>
       <Container maxWidth="lg">
         <Grid container spacing={3}>
-          {/* Left Section: About */}
-          <Grid item xs={12} sm={4}>
-            <Typography variant="h6" color="textPrimary" gutterBottom>
-              About
-            </Typography>
-            <Typography variant="body2" color="textSecondary" sx={{ color: 'white', fontWeight: 'bold', padding: '5px' }}>
-              <Link href="/pages/contact" color="inherit">
-                Contact Us
-              </Link>
-            </Typography>
-            <Typography variant="body2" color="textSecondary" sx={{ color: 'white', fontWeight: 'bold', padding: '5px' }}>
-              <Link href="/pages/about" color="inherit">
-                About Us
-              </Link>
-            </Typography>
+          {/* First Group: News App Logo, Subscribe, Social Media Icons */}
+          <Grid
+            item
+            xs={12}
+            sm={6}
+            style={{ display: "flex", alignItems: "center" }}
+          >
+            {/* Subscribe Text Field */}
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              {/* News App Logo */}
+              <img
+                src="/News-logo.jpg"
+                alt="News App Logo"
+                style={{
+                  width: "30%",
+                  marginRight: "10px",
+                  marginBottom: "5%",
+                }}
+              />
+              <TextField
+                id="subscribe"
+                variant="outlined"
+                size="small"
+                fullWidth
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <Button
+                        variant="contained"
+                        style={{
+                          backgroundColor: "blue",
+                          color: "white",
+                        }}
+                      >
+                        Subscribe
+                      </Button>
+                    </InputAdornment>
+                  ),
+                }}
+                sx={{ backgroundColor: "white", marginBottom: "10px" }}
+              />
+
+              {/* Social Media Icons */}
+              <div
+                style={{ display: "flex", marginLeft: "10px", color: "white" }}
+              >
+                <IconButton color="inherit" href="https://www.facebook.com">
+                  <Facebook />
+                </IconButton>
+                <IconButton color="inherit" href="https://www.twitter.com">
+                  <Twitter />
+                </IconButton>
+                <IconButton color="inherit" href="https://www.linkedin.com">
+                  <LinkedIn />
+                </IconButton>
+              </div>
+            </div>
           </Grid>
 
-          {/* Center Section: Address */}
-          <Grid item xs={12} sm={4}>
-            <Typography variant="h6" color="textPrimary" gutterBottom>
-              Mail Us
+          {/* Second Group: Categories */}
+          <Grid item xs={12} sm={6}>
+            {/* Categories Header */}
+            <Typography variant="h6" color="white" gutterBottom>
+              Categories
             </Typography>
-            <Typography variant="body2" color="textSecondary" sx={{ color: 'white', fontWeight: 'bold', padding: '5px' }}>
-              Bhuj,Gujarat,ABC area, 370030
-            </Typography>
-          </Grid>
 
-          {/* Right Section: Social */}
-          <Grid item xs={12} sm={4}>
-            <Typography variant="h6" color="textPrimary" gutterBottom>
-              Social
-            </Typography>
-            <Typography variant="body2" color="textSecondary" sx={{ color: 'white', fontWeight: 'bold', padding: '5px' }}>
-              <Link href="https://www.facebook.com" color="inherit">
-                Facebook
-              </Link>
-            </Typography>
-            <Typography variant="body2" color="textSecondary" sx={{ color: 'white', fontWeight: 'bold', padding: '5px' }}>
-              <Link href="https://www.twitter.com" color="inherit">
-                Twitter
-              </Link>
-            </Typography>
-            <Typography variant="body2" color="textSecondary" sx={{ color: 'white', fontWeight: 'bold', padding: '5px' }}>
-              <Link href="https://www.linkedin.com" color="inherit">
-                LinkedIn
-              </Link>
-            </Typography>
+            {/* Big Underline */}
+            <div
+              style={{ borderBottom: "2px solid white", marginBottom: "10px" }}
+            ></div>
+
+            {/* News Categories */}
+            <Grid container spacing={2}>
+              {/* Left Section */}
+              <Grid item xs={12} sm={4}>
+                <Link
+                  href="/pages/search/business"
+                  color="inherit"
+                  sx={{ textDecoration: "none" }}
+                >
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    sx={{
+                      color: "white",
+                      fontWeight: "bold",
+                      marginBottom: "20%",
+                    }}
+                  >
+                    Business
+                  </Typography>
+                </Link>
+                <Link
+                  href="/pages/search/sport"
+                  color="inherit"
+                  sx={{ textDecoration: "none" }}
+                >
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    sx={{
+                      color: "white",
+                      fontWeight: "bold",
+                      marginBottom: "20%",
+                    }}
+                  >
+                    Sports
+                  </Typography>
+                </Link>
+                <Link
+                  href="/pages/search/education"
+                  color="inherit"
+                  sx={{ textDecoration: "none" }}
+                >
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    sx={{
+                      color: "white",
+                      fontWeight: "bold",
+                      marginBottom: "20%",
+                    }}
+                  >
+                    Education
+                  </Typography>
+                </Link>
+                <Link
+                  href="/pages/search/career"
+                  color="inherit"
+                  sx={{ textDecoration: "none" }}
+                >
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    sx={{
+                      color: "white",
+                      fontWeight: "bold",
+                      marginBottom: "20%",
+                    }}
+                  >
+                    Career
+                  </Typography>
+                </Link>
+                {/* Add more categories as needed */}
+              </Grid>
+
+              {/* Middle Section */}
+              <Grid item xs={12} sm={4}>
+                <Link
+                  href="/pages/search/fun"
+                  color="inherit"
+                  sx={{ textDecoration: "none" }}
+                >
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    sx={{
+                      color: "white",
+                      fontWeight: "bold",
+                      marginBottom: "20%",
+                    }}
+                  >
+                    Fun
+                  </Typography>
+                </Link>
+                <Link
+                  href="/pages/search/bollywood"
+                  color="inherit"
+                  sx={{ textDecoration: "none" }}
+                >
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    sx={{
+                      color: "white",
+                      fontWeight: "bold",
+                      marginBottom: "20%",
+                    }}
+                  >
+                    bollywood
+                  </Typography>
+                </Link>
+                <Link
+                  href="/pages/search/hollywood"
+                  color="inherit"
+                  sx={{ textDecoration: "none" }}
+                >
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    sx={{
+                      color: "white",
+                      fontWeight: "bold",
+                      marginBottom: "20%",
+                    }}
+                  >
+                    hollywood
+                  </Typography>
+                </Link>
+                <Link
+                  href="/pages/search/movies"
+                  color="inherit"
+                  sx={{ textDecoration: "none" }}
+                >
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    sx={{
+                      color: "white",
+                      fontWeight: "bold",
+                      marginBottom: "20%",
+                    }}
+                  >
+                    Movies
+                  </Typography>
+                </Link>
+                {/* Add more categories as needed */}
+              </Grid>
+
+              {/* Right Section */}
+              <Grid item xs={12} sm={4}>
+                <Link
+                  href="/pages/search/politics"
+                  color="inherit"
+                  sx={{ textDecoration: "none" }}
+                >
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    sx={{
+                      color: "white",
+                      fontWeight: "bold",
+                      marginBottom: "20%",
+                    }}
+                  >
+                    Politics
+                  </Typography>
+                </Link>
+                <Link
+                  href="/pages/search/family"
+                  color="inherit"
+                  sx={{ textDecoration: "none" }}
+                >
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    sx={{
+                      color: "white",
+                      fontWeight: "bold",
+                      marginBottom: "20%",
+                    }}
+                  >
+                    Family
+                  </Typography>
+                </Link>
+                <Link
+                  href="/pages/search/social"
+                  color="inherit"
+                  sx={{ textDecoration: "none" }}
+                >
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    sx={{
+                      color: "white",
+                      fontWeight: "bold",
+                      marginBottom: "20%",
+                    }}
+                  >
+                    social
+                  </Typography>
+                </Link>
+                <Link
+                  href="/pages/search/Health"
+                  color="inherit"
+                  sx={{ textDecoration: "none" }}
+                >
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    sx={{
+                      color: "white",
+                      fontWeight: "bold",
+                      marginBottom: "20%",
+                    }}
+                  >
+                    Health
+                  </Typography>
+                </Link>
+                {/* Add more categories as needed */}
+              </Grid>
+            </Grid>
           </Grid>
         </Grid>
       </Container>
--- app/pages/search/[query]/page.js
+++ app/pages/search/[query]/page.js
@@ -1,4 +1,4 @@
-"use client";
+"use client"
 import React, { useEffect, useState } from "react";
 import { useParams, useRouter } from "next/navigation";
 import Card from "@mui/material/Card";
@@ -7,13 +7,14 @@ import CardMedia from "@mui/material/CardMedia";
 import TextField from "@mui/material/TextField";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
-import { Typography } from "@mui/material";
+import { Typography, Skeleton } from "@mui/material";
 import { getCategoryNews } from "@/app/service/getCategory";
 
 const SearchPage = () => {
   const router = useRouter();
   const { query } = useParams();
   const [searchResults, setSearchResults] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchSearchResults = async () => {
@@ -26,8 +27,10 @@ const SearchPage = () => {
 
         console.log("Search Results:", response.articles.results);
         setSearchResults(response.articles.results);
+        setLoading(false);
       } catch (error) {
         console.error("Error fetching search results:", error);
+        setLoading(false);
       }
     };
 
@@ -38,37 +41,78 @@ const SearchPage = () => {
     <Container maxWidth="xl" sx={{ mt: { xs: "25%", md: "15%", lg: "10%" } }}>
       <h1>Search Results for: {query}</h1>
       <Grid container spacing={3}>
-        {searchResults.map((result, index) => (
-          <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
-            <Card>
-              <CardHeader title={result.title} sx={{ background: "orange" }} />
-              {result.image && (
-                <CardMedia
-                  component="img"
-                  height="140"
-                  image={result.image}
-                  alt={result.title}
-                />
-              )}
-              <TextField
-                label="Description"
-                variant="outlined"
-                margin="normal"
-                fullWidth
-                multiline
-                readOnly
-                value={result.description}
-              />
-              <Typography>Source: {result.source.name}</Typography>
-              <p>
-                URL:{" "}
-                <a href={result.url} target="_blank" rel="noopener noreferrer">
-                  {result.url}
-                </a>
-              </p>
-            </Card>
-          </Grid>
-        ))}
+        {loading
+          ? // Skeleton Loading
+            Array.from({ length: 6 }).map((_, index) => (
+              <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
+                <Card
+                  sx={{
+                    height: "100%",
+                    display: "flex",
+                    flexDirection: "column",
+                  }}
+                >
+                  <Skeleton variant="rectangular" height={180} />
+                  <CardHeader title={<Skeleton />} />
+                  <Skeleton
+                    variant="text"
+                    height={20}
+                    width="80%"
+                    sx={{ margin: "auto" }}
+                  />
+                  <Skeleton
+                    variant="text"
+                    height={20}
+                    width="50%"
+                    sx={{ margin: "auto" }}
+                  />
+                </Card>
+              </Grid>
+            ))
+          : // Actual Search Results
+            searchResults.map((result, index) => (
+              <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
+                <Card
+                  sx={{
+                    height: "100%",
+                    display: "flex",
+                    flexDirection: "column",
+                  }}
+                >
+                  <CardHeader
+                    title={result.title}
+                    sx={{ background: "orange", textAlign: "center" }}
+                  />
+                  {result.image && (
+                    <CardMedia
+                      component="img"
+                      height="180"
+                      image={result.image}
+                      alt={result.title}
+                      sx={{ objectFit: "cover" }}
+                    />
+                  )}
+
+                  <Typography
+                    variant="body2"
+                    sx={{ paddingTop: "10%", textAlign: "center" }}
+                  >
+                    {result.date}
+                  </Typography>
+                  <Typography variant="body2" sx={{ textAlign: "center" }}>
+                    URL:{" "}
+                    <a
+                      href={result.url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      style={{ color: "blue", textDecoration: "underline" }}
+                    >
+                      {result.url}
+                    </a>
+                  </Typography>
+                </Card>
+              </Grid>
+            ))}
       </Grid>
     </Container>
   );
