513b76621e0d855ede032232cef2ebc4bf8d1d97
v_a_r_s_a_n_i <137641079+Varsani2520@users.noreply.github.com>
2024-01-30 12:14:48 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 export async function middleware(request) {
   const isAuthenticate = request.cookies.get("login")?.value === "true";
   const { pathname } = request.nextUrl;
-  const url = "http://localhost:3000";
+  const url = "https://news-web-app-delta.vercel.app/";
   // if user is unauthenticated and try to access profile page
   if (!isAuthenticate && pathname.startsWith("/pages/profile")) {
     return NextResponse.redirect(new URL(`${url}`, request.url));          
